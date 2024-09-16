e770e51b58fe822a591faea9e1750be253c24bd8
80dropz <153262187+80dropz@users.noreply.github.com>
2024-09-16 19:06:20 -0400

    Create driver-Optimizor.bat

---
+++ batch-scripts/driver-Optimizor.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Disable automatic page file management using WMIC
+defrag C: /O /U
+
+
+REM Optional: Pause to keep the window open and check output
+echo Finished Driver Opimization
+pause
