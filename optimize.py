dc278074cbd9904aaaaccd5c56038a202002c82d
80dropz <153262187+80dropz@users.noreply.github.com>
2024-12-03 18:00:15 -0500

    Update optimize.py

--- optimize.py
+++ optimize.py
@@ -1,20 +1,37 @@
-import customtkinter
-import subprocess
-import os
-import time
-from os import listdir
-import tkinter
-from datetime import datetime
-from elevate import elevate
+try:
+    import customtkinter
+    import subprocess
+    import os
+    import time
+    from os import listdir
+    import tkinter
+    from datetime import datetime
+    from elevate import elevate
+except:
+    subprocess.run("pip install customtkinter os tkinter datetime elevate", shell=True)
 try:
     elevate()
 except:
     print("Some features may be dissabled due to not running in administartor mode \n Some features may be dissabled due to not running in administartor mode \nSome features may be dissabled due to not running in administartor mode \nSome features may be dissabled due to not running in administartor mode \n")
 
+
+print(r"           /$$$$$$  /$$       /$$$$$$$$  /$$$$$$  /$$   /$$  /$$$$$$  /$$      /$$ /$$$$$$$$ /$$$$$$$$ /$$$$$$$ ")
+print(r"          /$$__  $$| $$      | $$_____/ /$$__  $$| $$$ | $$ /$$__  $$| $$  /$ | $$| $$_____/| $$_____/| $$__  $$")
+print(r"         | $$  \__/| $$      | $$      | $$  \ $$| $$$$| $$| $$  \__/| $$ /$$$| $$| $$      | $$      | $$  \ $$")
+print(r"         | $$      | $$      | $$$$$   | $$$$$$$$| $$ $$ $$|  $$$$$$ | $$/$$ $$ $$| $$$$$   | $$$$$   | $$$$$$$/")
+print(r"         | $$      | $$      | $$__/   | $$__  $$| $$  $$$$ \____  $$| $$$$_  $$$$| $$__/   | $$__/   | $$____/ ")
+print(r"         | $$    $$| $$      | $$      | $$  | $$| $$\  $$$ /$$  \ $$| $$$/ \  $$$| $$      | $$      | $$      ")
+print(r"         |  $$$$$$/| $$$$$$$$| $$$$$$$$| $$  | $$| $$ \  $$|  $$$$$$/| $$/   \  $$| $$$$$$$$| $$$$$$$$| $$      ")
+print(r"          \______/ |________/|________/|__/  |__/|__/  \__/ \______/ |__/     \__/|________/|________/|__/      ")
+
 #grabs the 4 letter handle of the user running the script
+filesfound = []
+commands = ["cache", "basic", "ram", "powercfg", "driver", "corrupted"]
 whoami = subprocess.run(["whoami"], capture_output=True, text=True, check=True)
 username = whoami.stdout.strip()
 user = username[-5:]
+cachelocation = [fr"C:\ProgramData\LGHUB\Cache",fr"C:\Users\{user}\AppData\Roaming\discord\Cache", fr"C:\Users\{user}\AppData\Roaming\battle.net\Cache", fr"C:\Users\{user}\AppData\Roaming\NVIDIA\Cache", fr"C:\Users\{user}\AppData\Roaming\G HUB\Cache", fr"C:\Users\{user}\AppData\Roaming\G HUB\Code Cache", fr"C:\Users\{user}\AppData\Roaming\G HUB\GPUCache",
+                 fr"C:\Users\{user}\AppData\Roaming\NVIDIA\ComputeCache", fr"C:\Users\{user}\AppData\Roaming\.minecraft\webcache2\Code Cache", fr"C:\Users\{user}\AppData\Roaming\.minecraft\webcache2\Cache", fr"C:\Users\{user}\AppData\Local\VALORANT\Saved\Crashes"]
 
 #starts the log appending on this script
 log = open("log.txt", "a")
@@ -25,59 +42,123 @@ filepath = filepathh[:-7]
 batch = f"{filepath}batch-scripts"
 #counts the number of optimizations made
 optimzations = 0
+sizeremoved = 0
 
 
-#just deletes temp folder and clears the recyling bin
+
+#I skidded this from "https://thepythoncode.com/article/get-directory-size-in-bytes-using-python" goat article just cant be asked to type allat SHOUTOUT "Abdeladim Fadheli"
+def get_directory_size(directory):
+
+    total = 0
+    try:
+        # print("[+] Getting the size of", directory)
+        for entry in os.scandir(directory):
+            if entry.is_file():
+                # if it's a file, use stat() function
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                # if it's a directory, recursively call this function
+                try:
+                    total += get_directory_size(entry.path)
+                except FileNotFoundError:
+                    pass
+    except NotADirectoryError:
+        # if `directory` isn't a directory, get the file size then
+        return os.path.getsize(directory)
+    except PermissionError:
+        # if for whatever reason we can't open the folder, return 0
+        return 0
+    return total
+
+
+#i also skidded this thank you sm "Abdeladim Fadheli" ---- "https://thepythoncode.com/article/get-directory-size-in-bytes-using-python"
+def get_size_format(b, factor=1024, suffix="B"):
+    """
+    Scale bytes to its proper byte format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
+
 def basiccleanup():
     global optimzations
+    global sizeremoved
     print("openeing tempfiles :()")
     try:
         for tempfile in listdir("C:\\Users\\acole\\AppData\\Local\\Temp"):
+            size = get_directory_size(tempfile)
             os.remove(tempfile)
-            optimzations = optimzations + 1
+            sizeremoved += size
+            optimzations += 1
         for tempfile2  in listdir("C:\\Windows\\Temp"):
+            size = get_directory_size(tempfile2)
             os.remove(tempfile2)
-            optimzations = optimzations + 1
+            sizeremoved += size
+            optimzations += 1
     except FileNotFoundError:
         print("temps have already been deleted")
         pass
-    subprocess.run("rd /s %systemdrive%\$Recycle.bin", shell=True)
-    optimzations = optimzations + 1
+    subprocess.run("rd /s %systemdrive%\\$Recycle.bin", shell=True)
+    optimzations += 1
 
-
-
-#should clear the cache of most common apps installed in computers
 def clearcache():
     global optimzations
-    if os.path.exists("C:\\ProgramData\\LGHUB"):
-        os.system(f"RMDIR /S /Q C:\\ProgramData\\LGHUB\\Cache")
-        optimzations = optimzations + 1
-        pass
-    if os.path.exists(f"C:\\Users\\{user}\\AppData\\Roaming\\discord"):
-        os.system(f"RMDIR /S /Q C:\\Users\\{user}\\AppData\\Roaming\\discord\\Cache")
-        optimzations = optimzations + 1
-        pass
-    if os.path.exists(f"C:\\Users\\{user}\\AppData\\Roaming\\battle.net"):
-        os.system(f"RMDIR /S /Q C:\\Users\\{user}\\AppData\\Roaming\\battle.net\\Cache")
-        optimzations = optimzations + 1
-        pass
-    if os.path.exists(f"C:\\Users\\{user}\\AppData\\Roaming\\NVIDIA"):
-        os.system(f"RMDIR /S /Q C:\\Users\\{user}\\AppData\\Roaming\\NVIDIA\\Cache")
-        optimzations = optimzations + 1
-        pass
+    global sizeremoved
+    for path in cachelocation:
+        print(f"Checking: {path}")
+        if os.path.exists(path):
+            size = get_directory_size(path)
+            try:
+                # Enclose the path in double quotes to handle spaces
+                subprocess.run(f'RMDIR /S /Q "{path}"', shell=True, check=True)
+                print(f"Removed: {path}")
+                sizeremoved += size
+                optimzations += 1
+            except subprocess.CalledProcessError as e:
+                print(f"Failed to remove {path}. Error: {e}")
+            except Exception as e:
+                print(f"Unknown error while removing {path}. Error: {e}")
+        else:
+            print(f"Path not found: {path}")
 
-    else:
-        print("none can be found to be cleared")
-        pass
+
+def systemscandef(directory):
+    global sizeremoved
+    for file in os.listdir(directory):
+        print(file)
+        if file.is_file():
+            if os.path not in filesfound:
+                filesfound.append(os.path.basename(file))
+            else:
+                size += get_directory_size(file)
+                os.remove(file)
+        elif file.is_dir():
+            systemscandef(os.path.join(directory, file))
+    print("Done!")
+
+
+
+
 
 def debloatlistcmd():
     global cachevar
+    global systemscan
+    global systemscanvar
     global cache
     if debloatlist.get():
         cachevar = tkinter.IntVar(value=0)
-        cache = customtkinter.CTkCheckBox(main, text="Clear Cache", onvalue=1, offvalue=0, variable=cachevar, width=30, height=15)
+        systemscanvar = tkinter.IntVar(value=0)
+        systemscan = customtkinter.CTkCheckBox(main, text="System Scan (Will take a while)", onvalue=1, offvalue=0, variable=cachevar, width=30, height=15)
+        systemscan.place(relx=.73, rely=.45)
+        cache = customtkinter.CTkCheckBox(main, text="Clear Cache", onvalue=1, offvalue=0, variable=systemscanvar, width=30, height=15)
         cache.place(relx=.73, rely=.35)
     else:
+        systemscan.destroy()
         cache.destroy()
 
 
@@ -92,16 +173,13 @@ def speedlist():
     global corrupted
 
     if speedlists.get():
-        #creates the variables for all the checkboxes in the function so when running will check for them and run the correct command
         powercfgvar = tkinter.IntVar(value=0)
         ramvar = tkinter.IntVar(value=0)
         drivervar = tkinter.IntVar(value=0)
         corruptedvar = tkinter.IntVar(value=0)
-        
-        #just creates the buttons, text, and placement of the buttons
         powercfg = customtkinter.CTkCheckBox(main, text="Preformance Mode", onvalue=1, offvalue=0, variable=powercfgvar, width=30, height=15)
         powercfg.place(relx=.125, rely=.35)
-        ram = customtkinter.CTkCheckBox(main, text="ram Optimization", onvalue=1, offvalue=0, variable=ramvar, width=30, height=15, command=lambda: print("ram is clicked"))
+        ram = customtkinter.CTkCheckBox(main, text="ram Optimization", onvalue=1, offvalue=0, variable=ramvar, width=30, height=15)
         ram.place(relx=.125, rely=.45)
         driver = customtkinter.CTkCheckBox(main, text="Driver Optimization", onvalue=1, offvalue=0, variable=drivervar, width=30, height=15)
         driver.place(relx=.125, rely=.55)
@@ -113,13 +191,13 @@ def speedlist():
         driver.destroy()
         corrupted.destroy()
 
-#should run the corrupted file remover from batch scripts
+
 def corruptedoptimization():
     global optimzations
     try:
         corrupted_optimizer = os.path.join("batch-scripts", "sfc-scan.bat")
         subprocess.run([corrupted_optimizer], shell=True)
-        optimzations = optimzations + 1
+        optimzations += 1
     except:
         log.write(f"Unkown error has accoured please report to 80dropz on twitter")
 
@@ -129,7 +207,7 @@ def driveroptimization():
     try:
         driver_optimizer = os.path.join("batch-scripts", "Driver-Optimizor.bat")
         subprocess.run([driver_optimizer], shell=True)
-        optimzations = optimzations + 1
+        optimzations += 1
     except FileNotFoundError:
         log.write(f"\n File not found (this is possibly due to not download zipfile from github) {time}")
     except:
@@ -144,7 +222,7 @@ def ramoptimization():
     try:
         ram_optimizer = os.path.join("batch-scripts", "Ram-Optimizor.bat")
         subprocess.run([ram_optimizer], shell=True)
-        optimzations = optimzations + 1
+        optimzations += 1
     except FileNotFoundError:
         log.write(f"\n File not found (this is possibly due to not download zipfile from github) {time}")
     except:
@@ -167,7 +245,7 @@ def high_performance():
                 os.system(f"powercfg -setactive {code}")
                 print(f"set power config to high performance at {time}")
                 log.write(f"\n set power config to high performance at {time}")
-                optimzations = optimzations + 1
+                optimzations += 1
     except subprocess.CalledProcessError as e:
         log.write(f"An error occurred while running powercfg: {e}")
     except Exception as e:
@@ -180,53 +258,58 @@ def high_performance():
 
 #checks if for every optimization to see if it has been checked or not
 def start():
-    try:
-        if cache.get():
-            log.write(f"cache ran noramlly at {time}")
-            clearcache()
-            print("Cleared Cache")
+        try:
+            if cache.get():
+                clearcache()
+                print("Cleared Cache")
+                pass
+        except:
             pass
-        if basic.get():
-            log.write(f"basic configuration ran noramlly at {time}")
-            print("basic error")
-            basiccleanup()
-            print("basic cleanup")
+        try:
+            if systemscan.get():
+                print("scanning system")
+                systemscandef("C:\\")
+                print("Scanned System")
+                pass
+        except:
             pass
-        if ram.get():
-            log.write(f"ram ran noramlly at {time}")
-            print("ram optimization")
-            ramoptimization()
+        try:
+            if basic.get():
+                print("basic error")
+                basiccleanup()
+                print("basic cleanup")
+                pass
+        except:
             pass
-        if powercfg.get():
-            log.write(f"powercfg ran noramlly at {time}")
-            high_performance()
-            print("powercfg")
+        try:
+            if ram.get():
+                print("ram optimization")
+                ramoptimization()
+                pass
+        except:
             pass
-        if driver.get():
-            log.write(f"driver ran noramlly at {time}")
-            driveroptimization()
-            print("Driver Optimized")
-        if corrupted.get():
-            log.write(f"Corrupted ran noramlly at {time}")
-            corruptedoptimization()
-            print("Corrupted Optimized")
+        try:
+            if powercfg.get():
+                high_performance()
+                print("powercfg")
+                pass
+        except:
             pass
-        else:
-            print("none selected")
+        try:
+            if driver.get():
+                driveroptimization()
+                print("Driver Optimized")
+        except:
             pass
-        log.write(f"\n finished optimization at {time} optimized {optimzations} things")
-        finished()
-    except:
-        log.write(f"no optimizations were found {time}")
-        print("none selected")
-        pass
-
-
-
-def finished():
-    print("\n \n \n \n \n \n \n \n YOUR PC HAS BEEN OPTIMIZED")
-
-
+        try:
+            if corrupted.get():
+                corruptedoptimization()
+                print("Corrupted Optimized")
+                pass
+        except:
+            pass
+        print("\n \n \n \n \n \n \n \n YOUR PC HAS BEEN OPTIMIZED")
+        print("Total Size Removed: ", get_size_format(sizeremoved))
 
 
 
